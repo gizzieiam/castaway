ea0c673d8680584cba4851e44e80d4c452a5c191
codenameGizmo <shavonnecarson94@gmail.com>
2021-02-13 13:34:06 -0800

    update player movement and added camera controller

---
+++ CameraControl.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    public float RotationSpeed = 1;
+    public Transform Target, Player;
+    public Transform Obstruction;
+    float zoomSpeed = 2f;
+    float mouseX, mouseY;
+
+    void Start()
+    {
+        Obstruction = Target;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+    void LateUpdate()
+    {
+        CamControl();
+        ViewObstructed();
+    }
+    void CamControl()
+    {
+        mouseX -= Input.GetAxis("Mouse X") * RotationSpeed;
+        mouseY += Input.GetAxis("Mouse Y") * RotationSpeed;
+        mouseY = Mathf.Clamp(mouseY, 0, 10);
+
+        transform.LookAt(Target);
+        if(Input.GetKey(KeyCode.LeftShift))
+        {
+            Target.rotation = Quaternion.Euler(mouseY, mouseX, 0);
+        }else
+        {
+            Target.rotation = Quaternion.Euler(mouseY, mouseX, 0);
+            Player.rotation = Quaternion.Euler(0, mouseX, 0);
+        }
+        
+    }
+
+    void ViewObstructed()
+    {
+        RaycastHit hit;
+
+        if(Physics.Raycast(transform.position, Target.position - transform.position, out hit, 0.5f))
+        {
+            if(hit.collider.gameObject.tag != "Player")
+            {
+                Obstruction = hit.transform;
+                Obstruction.gameObject.GetComponent<MeshRenderer>().shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.ShadowsOnly;
+                if(Vector3.Distance(Obstruction.position, transform.position) >= 2f && Vector3.Distance(transform.position, Target.position) >= 0.5f)
+                {
+                    transform.Translate(Vector3.forward * zoomSpeed * Time.deltaTime);
+                }
+            }else
+            {
+                Obstruction.gameObject.GetComponent<MeshRenderer>().shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.On;
+                if(Vector3.Distance(transform.position, Target.position) < 4.5f)
+                {
+                    transform.Translate(Vector3.back * zoomSpeed * Time.deltaTime);
+                }
+            }
+        }
+    }
+}
--- PlayerMovement.cs
+++ PlayerMovement.cs
@@ -4,20 +4,67 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float speed = 5f;
-    Rigidbody rb;
+public float speed;
+    public float time;
+    public bool isGrounded;
+    public Vector3 jump;
+    public int jumpCount;
+    public static bool isOver = false;
+    private Rigidbody self;
 
+
+
+    Vector3 velocity;
+    // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody>();    
+        self = GetComponent<Rigidbody>();
+        jump = new Vector3(0.0f, 2.0f, 0.0f);
+        jumpCount = 0;
+        time = 0.0f;
     }
 
+    // Update is called once per frame
     void Update()
     {
-        float xAxis = Input.GetAxisRaw("Horizontal");
-        float zAxis = Input.GetAxisRaw("Vertical");
+        PlayerMove();
+    }
 
-        rb.velocity = new Vector3(xAxis, rb.velocity.y, zAxis)*speed;
+    void PlayerMove()
+    {
+        if (isOver){
+            time = time + Time.deltaTime;
+        }
+        if(!isOver)
+        {
+            float moveX = Input.GetAxis("Horizontal");
+            float moveY = Input.GetAxis("Vertical");
 
+            Vector3 playerMovement = new Vector3(moveX, 0f, moveY) * speed * Time.deltaTime;
+            transform.Translate(playerMovement, Space.Self);
+
+            if(isGrounded)
+            {
+                transform.Translate(moveX * Time.deltaTime * speed, 0, moveY * Time.deltaTime);
+                if(Input.GetButtonDown("Jump"))
+                {
+                    jumpCount = jumpCount + 1;
+                self.AddForce(new Vector3(0, 7, 0), ForceMode.Impulse);
+                }
+                if(jumpCount == 2)
+                {
+                    isGrounded = false;
+                    jumpCount = 0;                
+                }
+            }
+        }
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Ground" || col.gameObject.tag == "Start")
+        {
+            isGrounded = true;
+        }
     }
 }
